290f6a7f75e76316cfe909a6db3b86222a2e7d4e
Ralf Globisch <rglobisch@csir.co.za>
2019-09-18 09:37:11 +0200

    Building all submodule filters

--- scripts/build.bat
+++ scripts/build.bat
@@ -24,6 +24,101 @@ call build.bat
 copy ..\build\Release\*.dll ..\..\build\Release\
 copy ..\build\Debug\*.dll ..\..\build\Debug\
 
+cd ..\..\H264DecoderFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\AudioMixingFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\CropFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\FrameSkippingFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\H264Source\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\I420ToRGB24Filter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\PicInPicFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\RGBtoI420Filter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\RGBtoYUV420Filter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\RotateFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\ScaleFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\Stereo2MonoFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\UtilityFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\VideoMixingFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\VirtualCam\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\VirtualMic\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\YUV420ToRGBFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\YUV444ToRGBFilter\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
+cd ..\..\YUVSource\scripts
+call build.bat
+copy ..\build\Release\*.dll ..\..\build\Release\
+copy ..\build\Debug\*.dll ..\..\build\Debug\
+
 ::cd ..\..\Opus\
 ::mkdir build
 ::cd build
@@ -45,3 +140,4 @@ copy ..\build\Debug\*.dll ..\..\build\Debug\
 ::cd ..\..\OpusEncoderFilter\scripts
 ::call build.bat
 
+cd ..\..\scripts\
