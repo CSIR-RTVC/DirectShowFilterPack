dcd840326ed943f66bc1659faad91f8940987f75
Ralf Globisch <rglobisch@csir.co.za>
2019-09-19 09:59:26 +0200

    Fixed path

--- setup/build_installer.bat
+++ setup/build_installer.bat
@@ -14,15 +14,12 @@ cd DirectShowFilterPack
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" /Build "Release" DirectShowFilterPack.sln
 set BUILD_STATUS=%ERRORLEVEL% 
 if not %BUILD_STATUS%==0 goto FAILED 
+cd ..
 
 echo "Installer built - copying installers to setup dir"
 @rem read version number from version file
 set /p Build=<version
+
 copy DirectShowFilterPack\DirectShowFilterPack\Release\DirectShowFilterPack.msi .\Setup_DirectShowFilterPack_%Build%.msi
 
-exit 0
-
-:FAILED
-echo "Failed to find VS install directory"
-exit 1
-
+@rem exit 0
